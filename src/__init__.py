3d91f85fb05296bcf05b6b5979655ddcbcbad991
Michael Peck <michaelwpeck2@gmail.com>
2019-10-08 13:52:06 -0400

    10/8/19 push1
    Got the restructuring working! Using Flask Blueprint

---
+++ src/__init__.py
@@ -0,0 +1,42 @@
+__author__ = 'michaelpeck'
+
+from flask import Flask, session
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+from src.config import Config
+from src.common.database import Database
+
+
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
+login_manager.login_message_category = 'info'
+mail = Mail()
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from src.main.routes import main
+    from src.users.routes import users
+    from src.posts.routes import posts
+    from src.processes.routes import processes
+    from src.errors.handlers import errors
+    app.register_blueprint(main)
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(processes)
+    app.register_blueprint(errors)
+
+    @app.before_first_request
+    def initialize_database():
+        Database.initialize()
+        session['email'] = None
+
+    return app
---
+++ src/processes/routes.py
@@ -0,0 +1,56 @@
+__author__ = 'michaelpeck'
+
+from flask import Flask, render_template, request, redirect, session, Blueprint
+from src.users.user import User
+from src.processes.models import Model
+from src.processes.calculation import Calculation
+
+
+processes = Blueprint('processes', __name__)
+
+
+@processes.route('/static_range')
+def static_range_template():
+    model = Model.get_by_id('5c5c5651b3144092ab2b8bf5f4daeada')
+    return render_template('static_range.html', model=model)
+
+@processes.route('/static_range/<string:model_id>')
+def static_range_template_model(model_id):
+    model = Model.get_by_id(model_id)
+    return render_template('static_range.html', model=model)
+
+@processes.route('/moving_average')
+def moving_average_template():
+    return render_template('moving_average.html')
+
+@processes.route('/weighted_moving_average')
+def weighted_moving_average_template():
+    return render_template('weighted_moving_average.html')
+
+@processes.route('/calc/static_range', methods=['POST'])
+def calc_data():
+    ticker = request.form['ticker']
+    period = request.form['period']
+    interval = request.form['interval']
+    money = request.form['money']
+    buy = request.form['buy']
+    sell = request.form['sell']
+    trade_cost = request.form['trade_cost']
+    model_name = request.form['model_name']
+
+    if session['email'] != None:
+        user = User.get_id_by_email(session['email'])
+        user_id = user.user_id
+    else:
+        user_id = "guest"
+
+    model_id = Model.create_model(ticker, period, interval, money, buy, sell, trade_cost, model_name, user_id)
+    transaction = Calculation.static_range("SR", ticker, period, interval, money, buy, sell, trade_cost, user_id, model_id)
+    url = "/results/" + transaction
+    return redirect(url)
+
+@processes.route('/results/<string:transaction_id>')
+def get_results(transaction_id):
+    results = Calculation.from_mongo(transaction_id)
+
+    return render_template('results.html', results=results)
